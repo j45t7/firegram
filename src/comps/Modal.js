facee0537f1f6e7d7622b9f212c5260eee7538ec
Justyna Marciniak <justy.marciniak@gmail.com>
2020-09-13 16:42:32 +0900

    add Modal open and close event

---
+++ src/comps/Modal.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Modal = ({ selectedImg, setSelectedImg }) => {
+  const handleClick = (e) => {
+    e.target.classList.contains('backdrop') && setSelectedImg(null)
+  }
+  return (
+    <div className='backdrop' onClick={handleClick}>
+      <img src={selectedImg} alt='large pic' />
+    </div>
+  )
+}
+
+export default Modal
