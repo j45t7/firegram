8d9f94d2c8131e62d39725a03c5e90513c89f873
Justyna Marciniak <justy.marciniak@gmail.com>
2020-09-13 15:57:30 +0900

    add ImageGrid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Title from './comps/Title';
+import React from 'react'
+import ImageGrid from './comps/ImageGrid'
+import Title from './comps/Title'
 import UploadForm from './comps/UploadForm'
 
 function App() {
   return (
-    <div className="App">
-      <Title/>
-      <UploadForm/>
+    <div className='App'>
+      <Title />
+      <UploadForm />
+      <ImageGrid />
     </div>
   )
 }
---
+++ src/comps/ImageGrid.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ImageGrid = () => {
+  return <div className='img-grid'>Images</div>
+}
+
+export default ImageGrid
